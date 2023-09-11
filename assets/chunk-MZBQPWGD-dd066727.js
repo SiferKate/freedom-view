5e0cdd063bfc53e96ad353c20fe6eb3163b25da1
SiferKate <117819942+SiferKate@users.noreply.github.com>
2023-09-12 00:41:59 +0400

    Add files via upload

---
+++ assets/chunk-MZBQPWGD-dd066727.js
@@ -0,0 +1,1 @@
+import{t as r}from"./index-b13a098d.js";import{j as i}from"./index-bcf0be10.js";var t=a=>i.jsx(r.div,{className:"chakra-stack__divider",...a,__css:{...a.__css,borderWidth:0,alignSelf:"stretch",borderColor:"inherit",width:"auto",height:"auto"}});t.displayName="StackDivider";export{t as S};
